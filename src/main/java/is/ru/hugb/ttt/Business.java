eb83b395a4f4be7b9125519200c2ab1634893788
Davíð Gunnarsson <eliseix@gmail.com>
2017-10-24 21:01:02 +0000

    format changes to business and businessTest, checkIfOccupied function and test made and passed

--- src/main/java/is/ru/hugb/ttt/Business.java
+++ src/main/java/is/ru/hugb/ttt/Business.java
@@ -16,17 +16,21 @@ public class Business{
 		return board;
 	}
 
-public static int[] convertToXY(int input)
-{
-			int x;
-			int y;
-			input = input - 1;
-      y = input % 3;
-			x = input / 3;
+	public static int[] convertToXY(int input){
+		int x;
+		int y;
+		input = input - 1;
+	    y = input % 3;
+		x = input / 3;
 
-			int[] arr = {x, y};
-			return arr;
+		int[] arr = {x, y};
+		return arr;
+	}
 
-}
-
+	public static boolean checkIfOccupied(char[][] board, int x, int y){
+		if(board[x][y] == 'X' || board[x][y] == 'O'){
+			return true;
+		}
+		return false;
+	}
 }
--- src/test/java/is/ru/hugb/ttt/BusinessTest.java
+++ src/test/java/is/ru/hugb/ttt/BusinessTest.java
@@ -7,8 +7,8 @@ import org.junit.rules.ExpectedException;
 
 public class BusinessTest{
 	@Test
-    public void testInitializeBoard() {
-        char[][] board = Business.initializeBoard();
+	public void testInitializeBoard() {
+    	char[][] board = Business.initializeBoard();
 
 		int cellCounter = 49;
 		for(int i = 0; i < 3; i++){
@@ -17,17 +17,29 @@ public class BusinessTest{
 				cellCounter++;
 			}
 		}
-    }
+	}
 
 		@Test
-		public void testConvertXtoY ()
-		{
+		public void testConvertXtoY(){
 			int[] array = Business.convertToXY(9);
 			int[] array2 = Business.convertToXY(1);
 
-			assertEquals(2 , array[0]);
+			assertEquals(2, array[0]);
 			assertEquals(2, array[1]);
 			assertEquals(0, array2[0]);
 			assertEquals(0, array2[1]);
 		}
+
+		@Test
+		public void testCheckIfOccupied(){
+			char[][] board = Business.initializeBoard();
+			board[0][0] = 'X';
+			board[2][2] = 'O';
+
+			assertEquals(true, Business.checkIfOccupied(board, 0, 0));
+			assertEquals(true, Business.checkIfOccupied(board, 2, 2));
+			assertEquals(false, Business.checkIfOccupied(board, 1, 2));
+		}
+
+
 }
