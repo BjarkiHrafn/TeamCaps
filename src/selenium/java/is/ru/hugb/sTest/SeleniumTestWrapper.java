fcb800eddacf51265ef050aa3dc5e5751143a29d
SnorriArinbjarnar <snorriarinbjarnar@gmail.com>
2017-10-28 21:02:33 +0100

    TravisSeleniumTest

--- src/selenium/java/is/ru/hugb/sTest/SeleniumTestWrapper.java
+++ src/selenium/java/is/ru/hugb/sTest/SeleniumTestWrapper.java
@@ -5,13 +5,12 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 public abstract class SeleniumTestWrapper {
 
-  //static ChromeDriver driver;
-  static WebDriver driver;
+  static ChromeDriver driver;
+  //static WebDriver driver;
   static String baseUrl;
   static String port;
 
@@ -21,7 +20,7 @@ public abstract class SeleniumTestWrapper {
   public static void openBrowser() {
 
     //driver = new ChromeDriver(dc);
-    driver = webDriver();
+    driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     port = System.getenv("PORT");
@@ -42,18 +41,6 @@ public abstract class SeleniumTestWrapper {
     driver.quit();
   }
 
-  public static WebDriver webDriver() {
-      final ChromeOptions chromeOptions = new ChromeOptions();
-      chromeOptions.setBinary(System.getProperty("google.chrome"));
-      chromeOptions.addArguments("--no-sandbox");
-      chromeOptions.addArguments("--headless");
-      chromeOptions.addArguments("--disable-gpu");
-      final DesiredCapabilities dc = new DesiredCapabilities();
-      dc.setJavascriptEnabled(true);
-      dc.setCapability(
-          ChromeOptions.CAPABILITY, chromeOptions
-      );
-      return new ChromeDriver(dc);
-  }
+
 
 }
